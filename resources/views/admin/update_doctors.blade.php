246ea2d1edd5d2b140af6b447c3062978da331e7
sakiulkawsar <mr.sakiulkawsar@gmail.com>
2026-01-22 23:58:16 +0600

    m

--- resources/views/admin/update_doctors.blade.php
+++ resources/views/admin/update_doctors.blade.php
@@ -2,54 +2,55 @@
 <base href="/public">
 
 @section('main')
-<form action="{{ route('post_update_doctors', $doctor->id) }}" method="post" enctype="multipart/form-data" style="padding-left: 100px;">
-    @csrf
 
-    @if(session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
+<div class="page-section">
+    <div class="container">
+      <h1 class="text-center wow fadeInUp">Get in Touch</h1>
 
-    <label>Fill the form</label>
+      <form class="contact-form mt-5" action="{{ route('post_update_doctors', $doctor->id) }}" method="post" enctype="multipart/form-data">
+        @csrf
+          @if(session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+@endif
+        
+        <div class="row mb-3">
+          <div class="col-sm-6 py-2 wow fadeInLeft">
+            <label for="fullName">Doctor Name</label>
+            <input type="text" id="fullName" name="doctors_name" value="{{ $doctor->doctors_name ?? '' }}" class="form-control" placeholder="Doctor name..">
+          </div>
+          <div class="col-sm-6 py-2 wow fadeInRight">
+            <label for="emailAddress">Doctor Phone number</label>
+            <input type="text" id="emailAddress" name="doctors_phone" value="{{ $doctor->doctors_phone ?? '' }}" class="form-control" placeholder="doctors phone..">
+          </div>
+          <div class="col-12 py-2 wow fadeInUp">
+            <label for="subject">Specialty</label>
+            <input type="text" id="subject" name="specialty" value="{{ $doctor->specialty ?? '' }}" class="form-control" placeholder="Enter specialty..">
+          </div>
 
-    <!-- Doctor Name -->
-    <div>
-        <input type="text" name="doctors_name" value="{{ $doctor->doctors_name ?? '' }}" placeholder="Doctor Name">
-    </div><br><br>
+             <div class="col-12 py-2 wow fadeInUp">
+            <label for="subject">Room Number</label>
+            <input type="text" id="subject" name="room_number" value="{{ $doctor->room_number ?? '' }}" class="form-control" placeholder="Enter room_number..">
+          </div>
 
-    <!-- Doctor Phone -->
-    <div>
-        <input type="text" name="doctors_phone" value="{{ $doctor->doctors_phone ?? '' }}" placeholder="Phone Number">
-    </div><br><br>
-
-    <!-- Specialty -->
-    <div>
-        <input type="text" name="specialty" value="{{ $doctor->specialty ?? '' }}" placeholder="Specialty">
-    </div><br><br>
-
-    <!-- Room Number -->
-    <div>
-        <input type="text" name="room_number" value="{{ $doctor->room_number ?? '' }}" placeholder="Room Number">
-    </div><br><br>
-
-    <!-- Old Image -->
-    <div>
-        <label style="border-radius: 12px; padding: 8px" class="bg bg-primary" for="doctor_image">Old Image</label>
-       <img src="{{ asset('storage/doctor_images/'.$doctor->doctor_image) }}" 
+          {{-- Old image --}}
+           <div class="col-12 py-2 wow fadeInUp">
+            <label for="subject">Old Image</label>
+           
+            <img src="{{ asset($doctor->doctor_image) }}" 
      alt="{{ $doctor->doctor_image }}" 
      style="width:100px;height:100px;">
-    </div><br><br>
+          </div>
 
-    <!-- Upload New Image -->
-    <div>
-        <label style="border-radius: 12px; padding: 8px" class="bg bg-primary" for="doctor_image">Upload Doctor's Image</label>
-        <input type="file" name="doctor_image">
-    </div><br><br>
-
-    <!-- Submit Button -->
-    <div>
-        <input type="submit" name="submit" value="Add Doctor" class="btn btn-success">
+            <div class="col-12 py-2 wow fadeInUp">
+            <label for="subject">New Image</label>
+            <input type="file" id="subject" name="doctor_image" class="form-control">
+          </div>
+         
+        </div>
+        <button type="submit" class="btn btn-primary wow zoomIn">Add-Doctor</button>
+      </form>
     </div>
-</form>
+  </div>
 @endsection
--- resources/views/doctor/update_patients.blade.php
+++ resources/views/doctor/update_patients.blade.php
@@ -2,51 +2,48 @@
 <base href="/public">
 
 @section('main')
-<form action="{{ route('post_update_patients', $patient->id) }}" method="post" enctype="multipart/form-data" style="padding-left: 100px;">
-    @csrf
 
-    @if(session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
+  <div class="page-section">
+    <div class="container">
+      <h1 class="text-center wow fadeInUp">Get in Touch</h1>
 
-    <label>Fill the form</label>
-
-    <!-- Doctor Name -->
-    <div>
-        <input type="text" name="patients_name" value="{{ $patient->patients_name ?? '' }}" placeholder="Doctor Name">
-    </div><br><br>
-
-    <!-- Doctor Phone -->
-    <div>
-        <input type="text" name="patients_phone" value="{{ $patient->patients_phone ?? '' }}" placeholder="Phone Number">
-    </div><br><br>
-
-    <!-- Specialty -->
-    <div>
-        <input type="text" name="problem" value="{{ $patient->problem ?? '' }}" placeholder="Specialty">
-    </div><br><br>
-
-   
-
-    <!-- Old Image -->
-    <div>
-        <label style="border-radius: 12px; padding: 8px" class="bg bg-primary" for="doctor_image">Old Image</label>
-       <img src="{{ asset('storage/patient_images/'.$patient->patient_image) }}" 
-     alt="{{ $patient->patient_image }}" 
-     style="width:100px;height:100px;">
-    </div><br><br>
-
-    <!-- Upload New Image -->
-    <div>
-        <label style="border-radius: 12px; padding: 8px" class="bg bg-primary" for="patient_image">Upload Patient's Image</label>
-        <input type="file" name="patient_image">
-    </div><br><br>
-
-    <!-- Submit Button -->
-    <div>
-        <input type="submit" name="submit" value="Add Patient" class="btn btn-success">
+      <form class="contact-form mt-5" action="{{ route('post_update_patients', $patient->id) }}" method="post" enctype="multipart/form-data">
+        @csrf
+          @if(session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
     </div>
-</form>
+@endif
+        
+        <div class="row mb-3">
+          <div class="col-sm-6 py-2 wow fadeInLeft">
+            <label for="fullName">Patient Name</label>
+            <input type="text" id="fullName" name="patients_name" value="{{ $patient->patients_name ?? '' }}" class="form-control" placeholder="Patient name..">
+          </div>
+          <div class="col-sm-6 py-2 wow fadeInRight">
+            <label for="emailAddress">Patient Phone number</label>
+            <input type="text" id="emailAddress" name="patients_phone" value="{{ $patient->patients_phone ?? '' }}" class="form-control" placeholder="Patient phone..">
+          </div>
+          <div class="col-12 py-2 wow fadeInUp">
+            <label for="subject">Problem</label>
+            <input type="text" id="subject" name="problem" value="{{ $patient->problem ?? '' }}" class="form-control" placeholder="Enter problem..">
+          </div>
+            {{-- old image --}}
+            <div class="col-12 py-2 wow fadeInUp">
+            <label for="subject">Old Image</label>
+           <img src="{{ asset($patient->patient_image) }}"
+     alt="Old Image"
+     style="width:100px;height:100px;">
+          </div>
+
+            <div class="col-12 py-2 wow fadeInUp">
+            <label for="subject">Upload Patient's Image</label>
+            <input type="file" id="subject" name="patient_image" class="form-control">
+          </div>
+         
+        </div>
+        <button type="submit" class="btn btn-primary wow zoomIn">Add-Patient</button>
+      </form>
+    </div>
+  </div>
 @endsection
